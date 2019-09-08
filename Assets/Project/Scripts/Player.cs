7435ded8318814aad18cab93bc8036537bbd7d0d
Markus Mayer <widemeadows@gmail.com>
2019-09-08 14:17:45 +0200

    Add player script for simple hazard collision

---
+++ Assets/Project/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    private int _hazardLayer;
+
+    private void Awake()
+    {
+        _hazardLayer = LayerMask.NameToLayer("Hazard");
+    }
+
+    private void OnCollisionEnter2D([NotNull] Collision2D other)
+    {
+        if (other.gameObject.layer == _hazardLayer)
+        {
+            Die();
+        }
+    }
+
+    private static void Die()
+    {
+        Debug.Log("Died!");
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+}
