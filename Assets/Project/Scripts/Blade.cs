cbc57256c49aac31fd01723d9f168b0b2de428a5
Markus Mayer <widemeadows@gmail.com>
2019-09-08 15:02:20 +0200

    Add sawblade prefab
    One interesting aspect here is the usage of (standard) gizmos
    right from the editor. By assigning label gizmos to the waypoints,
    we're able to see them without using any code or dummy sprite.

---
+++ Assets/Project/Scripts/Blade.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] waypoints;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float rotateSpeed = 200;
+
+    private int _targetIndex;
+
+    private void Update()
+    {
+        var tf = transform;
+        tf.Rotate(0, 0, -rotateSpeed * Time.deltaTime);
+
+
+        var pos = tf.position;
+        var targetPos = waypoints[_targetIndex].position;
+
+        pos = Vector2.MoveTowards(pos, targetPos, speed * Time.deltaTime);
+        tf.position = pos;
+        if (Vector2.Distance(pos, targetPos) <= 0.1f)
+        {
+            pos = targetPos;
+            _targetIndex = _targetIndex < waypoints.Length - 1 ? _targetIndex + 1 : 0;
+        }
+
+        tf.position = pos;
+    }
+}
