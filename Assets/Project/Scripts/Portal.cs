359d4d947e0ca63a40e4d1823eec7bec9d85ce31
Markus Mayer <widemeadows@gmail.com>
2019-09-08 18:07:11 +0200

    Add initial portal logic

---
+++ Assets/Project/Scripts/Portal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField]
+    private int portalId;
+
+    [SerializeField]
+    private int scene;
+
+    [SerializeField]
+    private Vector3 spawnPosition;
+
+    public int Id => portalId;
+
+    public int Scene => scene;
+
+    public Vector3 SpawnPosition => spawnPosition;
+
+    private void Awake()
+    {
+        // We expect the first (and only) child of a portal to be the spawn position.
+        Debug.Assert(transform.childCount == 1, "transform.childCount == 1");
+        spawnPosition = transform.GetChild(0).position;
+    }
+}
---
+++ Assets/Project/Scripts/PortalController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalController : MonoBehaviour
+{
+    private Portal[] _scenePortals;
+
+    private int _portalToSpawnAt;
+
+    private void Awake()
+    {
+        _scenePortals = FindObjectsOfType<Portal>();
+    }
+
+    public void SetSpawnPortal([NotNull] Portal portal)
+    {
+        _portalToSpawnAt = portal.Id;
+        PlayerPrefs.SetInt("PortalToSpawnAt", _portalToSpawnAt);
+        SceneManager.LoadScene(portal.Scene, LoadSceneMode.Single);
+    }
+
+    public Vector3 GetSpawnPosition()
+    {
+        _portalToSpawnAt = PlayerPrefs.GetInt("PortalToSpawnAt", -1);
+
+        var spawnPos = Vector3.zero;
+        foreach (var portal in _scenePortals)
+        {
+            if (portal.Id == _portalToSpawnAt)
+            {
+                spawnPos = portal.SpawnPosition;
+                return spawnPos;
+            }
+        }
+
+        throw new InvalidOperationException($"Missing portal reference for portal {_portalToSpawnAt}");
+    }
+}
